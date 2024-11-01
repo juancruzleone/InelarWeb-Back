1b43f8f9dd12fdec7a5e7637e0699cf04758abb1
Juan cruz Leone <juancruzleone@outlook.com>
2024-11-01 11:38:00 -0300

    Se modifico como se crean las ordenes

--- api/controllers/controller.api.checkout.js
+++ api/controllers/controller.api.checkout.js
@@ -33,37 +33,37 @@ const createOrder = async (req, res) => {
     }
 };
 
-const handleWebhook = async (req, res) => {
+const handleSuccess = async (req, res) => {
     try {
-        const { type, data } = req.body;
+        // Aquí se debe obtener la información de la transacción con el ID que MercadoPago devuelve
+        const { payment_id, external_reference } = req.query;
 
-        if (type === 'payment') {
-            const paymentId = data.id;
-            const paymentInfo = await mercadoPago.payment.findById(paymentId);
+        const mercadoPago = new MercadoPagoConfig({ accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN });
+        const paymentInfo = await mercadoPago.payment.findById(payment_id);
 
-            if (paymentInfo.body.status === 'approved') {
-                const orden = {
-                    userId: paymentInfo.body.external_reference,
-                    items: paymentInfo.body.additional_info.items,
-                    total: paymentInfo.body.transaction_details.total_paid_amount,
-                    estado: 'success',
-                    createdAt: new Date()
-                };
+        if (paymentInfo.body.status === 'approved') {
+            // Insertar la orden solo si el estado es 'approved'
+            const orden = {
+                userId: external_reference,
+                items: paymentInfo.body.additional_info.items,
+                total: paymentInfo.body.transaction_details.total_paid_amount,
+                estado: 'success',
+                createdAt: new Date()
+            };
 
-                const ordersCollection = db.collection('ordenes');
-                await ordersCollection.insertOne(orden);
-                console.log('Orden insertada con éxito');
-            }
+            const ordersCollection = db.collection('ordenes');
+            await ordersCollection.insertOne(orden);
+            console.log('Orden insertada con éxito');
         }
 
-        res.sendStatus(200);
+        res.redirect('/carrito?status=success');
     } catch (error) {
-        console.error('Error handling webhook:', error);
+        console.error('Error handling success:', error);
         res.status(500).json({ error: error.message });
     }
 };
 
 export {
     createOrder,
-    handleWebhook
+    handleSuccess
 };
