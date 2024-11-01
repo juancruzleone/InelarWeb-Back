b7874ffc9d6b283786ca6a86866373b0ade4feeb
Juan cruz Leone <juancruzleone@outlook.com>
2024-11-01 19:28:24 -0300

    Se elimino el cambio de estado en las ordenes

--- api/routes/route.api.checkout.js
+++ api/routes/route.api.checkout.js
@@ -1,14 +1,15 @@
 import { Router } from 'express';
-import { createOrder, handleWebhook, updateOrderStatus } from '../controllers/controller.api.checkout.js';
-import { isAdmin } from '../../middleware/auth.role.middleware.js';
+import { createOrder, handleWebhook } from '../controllers/controller.api.checkout.js';
+
 
 const router = Router();
+
 
 router.post('/create-order', createOrder);
 router.get('/success', (req, res) => res.send('Success'));
 router.get('/failure', (req, res) => res.send('Failure'));
 router.get('/pending', (req, res) => res.send('Pending'));
 router.post('/webhook', handleWebhook);
-router.put('/order/:orderId/estado', [isAdmin], updateOrderStatus);
+
 
 export default router;
